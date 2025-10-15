622b3f72dedca5bc43e5654ca31a14cf2a429594
darthplagueiswise <zelihmannradan@gmail.com>
2025-10-15 15:18:17 -0300

    Refactor LiquidGlassOn.m for method swizzling

--- src/LiquidGlassOn.m
+++ src/LiquidGlassOn.m
@@ -1,55 +1,42 @@
+// src/LiquidGlassOn.m
 #import <Foundation/Foundation.h>
-#import <objc/runtime.h>
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 
-// Replacement implementation for -[WDSLiquidGlass isEnabled]
-static BOOL LG_isEnabled(id self, SEL _cmd) {
-    // Always report that Liquid Glass is enabled
-    return YES;
+static BOOL lg_isEnabled(id self, SEL _cmd) { return YES; }
+
+static void (*orig_setWindowLevel)(UIWindow *self, SEL _cmd, CGFloat level);
+static void lg_setWindowLevel(UIWindow *self, SEL _cmd, CGFloat level) {
+    if (level > 0.0) level = 0.0;            // rebaixa overlays abusados
+    if (orig_setWindowLevel) orig_setWindowLevel(self, _cmd, level);
 }
 
-// Replacement implementation for -[UIWindow setWindowLevel:]
-static void LG_setWindowLevel(UIWindow *self, SEL _cmd, CGFloat level) {
-    // Clamp any window level above normal back to normal so overlays can't hide Liquid Glass
-    if (level > 0.0) {
-        level = 0.0;
-    }
-    // Call original implementation
-    void (*orig)(id, SEL, CGFloat) = (void (*)(id, SEL, CGFloat))objc_getAssociatedObject(self, _cmd);
-    if (orig) {
-        orig(self, _cmd, level);
-    }
-}
-
-// Constructor runs when the dylib is loaded
 __attribute__((constructor))
-static void LG_initialize(void) {
-    // Persistently set the preferences to enable Liquid Glass
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:@"UserDefaults" forKey:@"WALiquidGlassOverrideMethod"];
-    [defaults setBool:YES forKey:@"WAOverrideLiquidGlassEnabled"];
-    [defaults setBool:YES forKey:@"LiquidGlassEnabled"];
-    [defaults synchronize];
+static void lg_boot(void) {
+    // 1) Força caminho "UserDefaults" + booleans ON
+    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    [d setObject:@"UserDefaults" forKey:@"WALiquidGlassOverrideMethod"];
+    [d setBool:YES forKey:@"WAOverrideLiquidGlassEnabled"];
+    [d setBool:YES forKey:@"LiquidGlassEnabled"];
+    [d synchronize];
 
-    // Swizzle +[WDSLiquidGlass isEnabled] to always return YES
-    Class lgClass = NSClassFromString(@"WDSLiquidGlass");
-    SEL enabledSel = @selector(isEnabled);
-    Method enabledMethod = class_getClassMethod(lgClass, enabledSel);
-    if (enabledMethod) {
-        IMP originalIMP = method_getImplementation(enabledMethod);
-        // Associate original IMP to retrieve in replacement if needed
-        objc_setAssociatedObject(lgClass, enabledSel, (id)originalIMP, OBJC_ASSOCIATION_ASSIGN);
-        method_setImplementation(enabledMethod, (IMP)LG_isEnabled);
+    // 2) +[WDSLiquidGlass isEnabled] -> sempre YES
+    Class LG = objc_getClass("WDSLiquidGlass");
+    if (LG) {
+        SEL sel = sel_registerName("isEnabled");
+        Method m = class_getClassMethod(LG, sel);
+        if (m) method_setImplementation(m, (IMP)lg_isEnabled);
     }
 
-    // Swizzle -[UIWindow setWindowLevel:] to clamp overlay levels
-    Class windowClass = [UIWindow class];
-    SEL setLevelSel = @selector(setWindowLevel:);
-    Method setLevelMethod = class_getInstanceMethod(windowClass, setLevelSel);
-    if (setLevelMethod) {
-        IMP originalSetLevel = method_getImplementation(setLevelMethod);
-        // Store original implementation so our replacement can call through
-        objc_setAssociatedObject(windowClass, setLevelSel, (id)originalSetLevel, OBJC_ASSOCIATION_ASSIGN);
-        method_setImplementation(setLevelMethod, (IMP)LG_setWindowLevel);
+    // 3) -[UIWindow setWindowLevel:] -> clamp
+    Class UIW = objc_getClass("UIWindow"); 
+    if (UIW) {
+        SEL sel = sel_registerName("setWindowLevel:");
+        Method m = class_getInstanceMethod(UIW, sel);
+        if (m) {
+            IMP oldImp = method_getImplementation(m);
+            orig_setWindowLevel = (void(*)(UIWindow*,SEL,CGFloat))oldImp;
+            method_setImplementation(m, (IMP)lg_setWindowLevel);
+        }
     }
 }
