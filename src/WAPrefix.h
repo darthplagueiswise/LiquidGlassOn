bdea2b9803cf90071a7eb5e3f370d01ae7210dd1
euoradan <zelihmannradan@gmail.com>
2026-05-14 08:55:18 -0300

    Add WhatsApp tweak preference keys

--- src/WAPrefix.h
+++ src/WAPrefix.h
@@ -8,6 +8,9 @@
 #define WALog(fmt, ...) NSLog(@"[LiquidGlassOn] " fmt, ##__VA_ARGS__)
 
 #define WA_PREF_KEYCHAIN_REWRITE @"wa_sideload_keychain_rewrite_enabled"
+#define WA_PREF_KEYCHAIN_OBSERVER @"wa_keychain_observer_enabled"
 #define WA_PREF_EMPLOYEE_MASTER @"wa_employee_master"
 #define WA_PREF_AB_OBSERVER @"wa_abprops_observer_enabled"
 #define WA_PREF_LIQUID_GLASS @"wa_liquid_glass_enabled"
+#define WA_PREF_LIQUID_GLASS_USERDEFAULTS @"wa_liquid_glass_userdefaults_overrides"
+#define WA_PREF_LIQUID_GLASS_METHOD_HOOKS @"wa_liquid_glass_method_hooks"
