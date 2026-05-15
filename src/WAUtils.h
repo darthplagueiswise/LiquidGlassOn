17f18ba2d1f27a730d687d4b32449dab2a78cd7b
euoradan <zelihmannradan@gmail.com>
2026-05-14 21:03:34 -0300

    Fix WAUtils C linkage for Objective-C++ hooks

--- src/WAUtils.h
+++ src/WAUtils.h
@@ -3,6 +3,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 BOOL WAEnabled(NSString *key);
 void WASetEnabled(NSString *key, BOOL enabled);
 void WARegisterDefaults(void);
@@ -12,5 +16,9 @@ Class WAFindClassByNameFragment(NSString *fragment);
 NSArray<Class> *WAClassesMatchingFragments(NSArray<NSString *> *fragments, NSUInteger limit);
 BOOL WAInstanceRespondsTo(Class cls, SEL sel);
 BOOL WAClassRespondsTo(Class cls, SEL sel);
+
+#ifdef __cplusplus
+}
+#endif
 
 NS_ASSUME_NONNULL_END
