96c46adfe81d2e93689d351cbc199166a3b5ad89
euoradan <zelihmannradan@gmail.com>
2026-05-17 18:59:53 -0300

    Declare runtime and catalog browser classes for dev4 build

--- src/Menu/WAGramMenuVC.h
+++ src/Menu/WAGramMenuVC.h
@@ -4,26 +4,40 @@
 @interface WAGramMenuVC : UITableViewController
 @end
 
-#pragma once
-#import <UIKit/UIKit.h>
+@interface WAGRABFlagBrowserVC : UITableViewController
+- (instancetype)initWithTitle:(NSString *)t flags:(NSArray<NSString *> *)flags;
+- (void)reload;
+- (void)updateTitle;
+@end
+
+@interface WAGRRuntimeMethodBrowserVC : UITableViewController
+- (instancetype)initWithTitle:(NSString *)title tokens:(NSArray<NSString *> *)tokens;
+@end
+
+@interface WAGRWAABCatalogBrowserVC : UITableViewController
+@end
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 // WAAB Observer
 void      WAGRWAABEnsureHooksInstalled(void);
 BOOL      WAGRWAABOriginalBoolForFlag(NSString *flag, BOOL *knownOut);
 NSString *WAGRWAABDiagnosticText(void);
 NSString *WAGRABObsLog(void);
 void      WAGRABObsClear(void);
+
 // LiquidGlass
 void      WAGRLGPrefsDidChange(void);
 BOOL      WAGRLGSystemDefaultEnabled(void);
 BOOL      WAGRLGEffectiveEnabled(void);
 NSString *WAGRLGDiagnosticText(void);
+
 // Dogfood / Employee
 void      WAGRDogfoodEnsureHooksInstalled(void);
 NSString *WAGRDogfoodDiagnosticText(void);
+
 // Aura
 void      WAGRAuraEnsureHooksInstalled(void);
 void      WAGRAuraActivateAllFlags(void);
@@ -32,21 +46,22 @@ NSString *WAGRAuraDiagnostic(void);
 BOOL      WAGRPushAuraThemesVC(UIViewController *from);
 BOOL      WAGRPushAuraIconsVC(UIViewController *from);
 BOOL      WAGRPushAuraRingtonesVC(UIViewController *from);
+
 // Aura Gating (subscription-state hooks)
 void      WAGRAuraGatingEnsureHooksInstalled(void);
 void      WAGRAuraGatingActivate(BOOL on);
+
 // Context / Debug
 void      WAGRContextEnsureHooksInstalled(void);
 void      WAGRContextSetSimulateDebug(BOOL on);
 BOOL      WAGRContextIsSimulatingDebug(void);
 BOOL      WAGRContextIsDebugMenuForced(void);
 NSString *WAGRContextDiagnosticText(void);
+
+// Runtime exact BOOL getter hooks
+void      WAGRRuntimeRestorePersistedOverrides(void);
+NSString *WAGRRuntimeBoolDiagnosticText(void);
+
 #ifdef __cplusplus
 }
 #endif
-
-@interface WAGRABFlagBrowserVC : UITableViewController
-- (instancetype)initWithTitle:(NSString *)t flags:(NSArray<NSString *> *)flags;
-- (void)reload;
-- (void)updateTitle;
-@end
